24af3d6da1506f46727dbafc397e5393cb2177ce
박병욱 <quddnr2688@gmail.com>
2023-11-30 13:57:22 +0900

    어드민 헤더 사이드바 추가 어드민 회원 조회/ 수정 페이지 추가

--- javaWorkSpace/nongra/src/main/webapp/resources/js/common/aside/aside_admin.js
+++ javaWorkSpace/nongra/src/main/webapp/resources/js/common/aside/aside_admin.js
@@ -1,15 +1,13 @@
-   // document.createElement(".delete");
-   const aside = document.querySelector("aside");
-      
-
-        
+// document.createElement(".delete");
+const aside = document.querySelector("aside");
 
   window.addEventListener('scroll', function() {
     const scrollPosition = window.pageYOffset;
     console.log(aside.clientHeight);
+
     if (headerHeight <= scrollPosition) {
-		console.log(aside.clientHeight);
       aside.classList.add('fixed');
+      
     } else {
       aside.classList.remove('fixed');
     }
