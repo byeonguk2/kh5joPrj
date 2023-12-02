8d71c46bb7c18f0278db68a7723e460ec5fb256e
박병욱 <quddnr2688@gmail.com>
2023-12-02 22:04:35 +0900

    소비자 회원 정보 조회 진행중

--- javaWorkSpace/nongra/src/main/webapp/resources/js/common/aside/aside_seller.js
+++ javaWorkSpace/nongra/src/main/webapp/resources/js/common/aside/aside_seller.js
@@ -8,7 +8,7 @@ const aside = document.querySelector("aside");
             console.log(aside);
         });
        
-        const side = document.querySelector(".sitem1 > .side-icon2");   
+        const side = document.querySelector(".sitem1 > .down_icon");   
         side.addEventListener("click",()=>{
             const menu = document.querySelectorAll(".menu"); 
             side.classList.toggle("rotate");
@@ -16,12 +16,12 @@ const aside = document.querySelector("aside");
                 menu[i].classList.toggle("plus-menu");
             }
         });
-        const side2 = document.querySelector(".sitem2 > .side-icon2");   
+        const side2 = document.querySelector(".sitem2 > .down_icon");   
         side2.addEventListener("click",()=>{
             const menu = document.querySelectorAll(".menu"); 
             side2.classList.toggle("rotate");
         });
-        const side3 = document.querySelector(".sitem3 > .side-icon2");  
+        const side3 = document.querySelector(".sitem3 > .down_icon");  
         console.log(side2); 
         side3.addEventListener("click",()=>{
             const menu = document.querySelectorAll(".menu");
@@ -30,32 +30,32 @@ const aside = document.querySelector("aside");
                 menu[i].classList.toggle("plus-menu");
             }
         });
-        const side4 = document.querySelector(".sitem4 > .side-icon2");   
+        const side4 = document.querySelector(".sitem4 > .down_icon");   
         side4.addEventListener("click",()=>{
             const menu = document.querySelectorAll(".menu"); 
             side4.classList.toggle("rotate");
         });
-        const side5 = document.querySelector(".sitem5 > .side-icon2");   
+        const side5 = document.querySelector(".sitem5 > .down_icon");   
         side5.addEventListener("click",()=>{
             const menu = document.querySelectorAll(".menu"); 
             side5.classList.toggle("rotate");
         });
-        const side6 = document.querySelector(".sitem6 > .side-icon2");   
+        const side6 = document.querySelector(".sitem6 > .down_icon");   
         side6.addEventListener("click",()=>{
             const menu = document.querySelectorAll(".menu"); 
             side6.classList.toggle("rotate");
         });
-        const side7 = document.querySelector(".sitem7 > .side-icon2");   
+        const side7 = document.querySelector(".sitem7 > .down_icon");   
         side7.addEventListener("click",()=>{
             const menu = document.querySelectorAll(".menu"); 
             side7.classList.toggle("rotate");
         });
-        const side8 = document.querySelector(".sitem8 > .side-icon2");   
+        const side8 = document.querySelector(".sitem8 > .down_icon");   
         side8.addEventListener("click",()=>{
             const menu = document.querySelectorAll(".menu"); 
             side8.classList.toggle("rotate");
         });
-        const side9 = document.querySelector(".sitem9 > .side-icon2");  
+        const side9 = document.querySelector(".sitem9 > .down_icon");  
         console.log(side3); 
         side9.addEventListener("click",()=>{
             const menu = document.querySelectorAll(".menu");
@@ -64,12 +64,12 @@ const aside = document.querySelector("aside");
                 menu[i].classList.toggle("plus-menu");
             }
         });
-        const side10 = document.querySelector(".sitem10 > .side-icon2");   
+        const side10 = document.querySelector(".sitem10 > .down_icon");   
         side10.addEventListener("click",()=>{
             const menu = document.querySelectorAll(".menu"); 
             side10.classList.toggle("rotate");
         });
-        const side11 = document.querySelector(".sitem11 > .side-icon2");   
+        const side11 = document.querySelector(".sitem11 > .down_icon");   
         side11.addEventListener("click",()=>{
             const menu = document.querySelectorAll(".menu"); 
             side11.classList.toggle("rotate");
