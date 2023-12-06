021beb1bcb5913a92ffc143eabf09a881dda6a9e
moooook\moook <lolipop6266@gmail.com>
2023-12-06 19:02:17 +0900

    초이스

---
+++ javaWorkSpace/nongra/src/main/java/com/kh/app/choice/MemberOrSeller.java
@@ -0,0 +1,17 @@
+package com.kh.app.choice;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/choice/login")
+public class MemberOrSeller extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/views/common/joinChoice/memberOrSeller.jsp").forward(req, resp);
+	}
+}
