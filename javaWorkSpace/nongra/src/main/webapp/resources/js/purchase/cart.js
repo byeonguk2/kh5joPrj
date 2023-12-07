beb6e2caa66ce709439893a32ec485b4a8954e93
DESKTOP-I3EC9MB\user1 <gksdbswl951@gmail.com>
2023-12-07 17:50:31 +0900

    HYJ_1207_1749 장바구니 + 버튼 AJAX실행 자바로 데이터 넘어가는거 확인

--- javaWorkSpace/nongra/src/main/webapp/resources/js/purchase/cart.js
+++ javaWorkSpace/nongra/src/main/webapp/resources/js/purchase/cart.js
@@ -10,7 +10,17 @@ function removeBtnClick(btn){
 }
 
 
-
+/* 감소버튼AJAX */
+function ea(event){
+	const cartBreakDownNo = event.currentTarget.parentNode.parentNode.parentNode.children[2].children[0].children[2].innerText;
+	const cartBreakDownEA = event.currentTarget.parentNode.children[1].innerText;
+/*	//서버한테 요청을 보내기
+	fetch("http://127.0.0.1:8888/nongra/cart/AJAX?cartBreakDownNo=" + cartBreakDownNo + "&cartBreakDownEa=" + )
+	.then( (resp) => { return resp.json() } )
+	.then( (x) => { 
+		console.log(x);
+	} );*/
+}
 
 
 /* 수량 증가,감소 버튼 */
@@ -21,13 +31,40 @@ for(let i = 0; i < plusBtnArr.length; ++i){
 	plusBtnArr[i].addEventListener('click', function(event){
 		const upDownNo = event.currentTarget.parentNode.children[1];
 		upDownNo.innerText = parseInt(upDownNo.innerText) + 1;
-	});
+		if(upDownNo.innerText == 2){
+		event.currentTarget.parentNode.children[0].children[0].classList.remove('disabled');
+		}
+	
+		const cartBreakDownNo = event.currentTarget.parentNode.parentNode.parentNode.children[2].children[0].children[2].innerText;
+		const cartBreakDownEA = event.currentTarget.parentNode.children[1].innerText;	
+		
+		//서버한테 요청을 보내기
+		fetch("http://127.0.0.1:8888/nongra/cart/AJAX?cartBreakDownNo=" + cartBreakDownNo + "&cartBreakDownEa=" + cartBreakDownEA)
+		.then( (resp) => { return resp.json() } )
+		.then( (x) => { 
+			console.log(x);
+		} );
+		
+		
+	});//plusBtnArr이벤트
 	
 	minusBtnArr[i].addEventListener('click', function(event){
 		const upDownNo = event.currentTarget.parentNode.children[1];
-		upDownNo.innerText = parseInt(upDownNo.innerText) - 1;
-	});
-}
+		if(upDownNo.innerText == 1){
+			upDownNo.innerText = upDownNo.innerText; 			
+		}else{
+			upDownNo.innerText = parseInt(upDownNo.innerText) - 1;
+			if(upDownNo.innerText == 1){
+			event.currentTarget.children[0].classList.add('disabled');
+			}
+		}
+	});//minusBtnArr이벤트
+}//for문
+
+
+
+
+
 
 
 
