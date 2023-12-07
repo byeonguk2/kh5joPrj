7f143c3cc01f588bd56761a031c748413d80b948
DESKTOP-I3EC9MB\user1 <gksdbswl951@gmail.com>
2023-12-07 15:02:32 +0900

    HYJ_1207_1501 장바구니 수량버튼 구현

--- javaWorkSpace/nongra/src/main/webapp/resources/js/purchase/cart.js
+++ javaWorkSpace/nongra/src/main/webapp/resources/js/purchase/cart.js
@@ -1,20 +1,3 @@
-/*수량버튼*/
-/*const down = document.querySelector('.downBtn');
-down.addEventListener('click',()=>{
-    let number = document.querySelector('.upDownNo').innerText;
-    if(number == 0){
-        document.querySelector('.upDownNo').innerText = number;
-    }else{
-        document.querySelector('.upDownNo').innerText = number - 1;
-    }
-
-})*/
-const up = document.querySelector('.upBtn');
-up.addEventListener('click',()=>{
-    let number = document.querySelector('.upDownNo').innerText;
-    document.querySelector('.upDownNo').innerText = parseInt(number) + 1;
-})
-
 /*한줄씩 요소 선택*/
 const trArr = document.querySelectorAll("table > tbody > tr");
 
@@ -30,34 +13,20 @@ function removeBtnClick(btn){
 
 
 
-
-/* 감소 */
-for(let i = 0; i < trArr.length; ++i){
-	/*-버튼 요소*/
-	console.log('for1');
-	console.log(i);
-	console.log('for2');
-	trArr[i].children[3].children[0].children[0].addEventListener('click',down(i))
-
-}
-function down(i){
+/* 수량 증가,감소 버튼 */
+/* 증가 */
+const plusBtnArr = document.querySelectorAll(".upBtn");
+const minusBtnArr = document.querySelectorAll(".downBtn");
+for(let i = 0; i < plusBtnArr.length; ++i){
+	plusBtnArr[i].addEventListener('click', function(event){
+		const upDownNo = event.currentTarget.parentNode.children[1];
+		upDownNo.innerText = parseInt(upDownNo.innerText) + 1;
+	});
 	
-	console.log('down1');
-	console.log(i);
-	console.log('down2');
-	let number = trArr[i].children[3].children[0].children[1].innerText;
-	console.log(number);
-	console.log('down3');
-	if(number == 0){
-		trArr[i].children[3].children[0].children[1].innerText = number;
-	}else{
-		trArr[i].children[3].children[0].children[1].innerText = number - 1;
-	}	
-}
-
-for(let i = 0; i < trArr.length; ++i){
-	/*+버튼 요소*/
-	trArr[0].children[3].children[0].children[2]
+	minusBtnArr[i].addEventListener('click', function(event){
+		const upDownNo = event.currentTarget.parentNode.children[1];
+		upDownNo.innerText = parseInt(upDownNo.innerText) - 1;
+	});
 }
 
 
