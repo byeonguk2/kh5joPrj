a1d8172207379439941646b8d2b58af1ff2e8985
박병욱 <quddnr2688@gmail.com>
2023-12-13 19:55:49 +0900

    ㅇㄴㅁㅇㅁㄴㅇ

--- javaWorkSpace/nongra/src/main/java/com/kh/app/home/HomeServlet.java
+++ javaWorkSpace/nongra/src/main/java/com/kh/app/home/HomeServlet.java
@@ -1,6 +1,8 @@
 package com.kh.app.home;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,12 +10,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.kh.app.home.service.HomeService;
+import com.kh.app.sales.vo.SalesVo;
+
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet{
 
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(req, resp);
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		try {
+			HomeService hs = new HomeService();
+			Map<String , Object> map = hs.selectItemList();
+			
+			req.setAttribute("map", map);
+			
+			req.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(req, resp);
+			
+		}catch (Exception e) {
+			
+			e.printStackTrace();
+			req.setAttribute("alret", "홈페이지 접속 실패..");
+			req.getRequestDispatcher("/WEB-INF/views/common/error.jsp").forward(req, resp);
+			
+		}
+		
+		
 	}
 
 }
---
+++ javaWorkSpace/nongra/src/main/java/com/kh/app/home/service/HomeService.java
@@ -0,0 +1,43 @@
+package com.kh.app.home.service;
+
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.kh.app.home.dao.HomeDao;
+import com.kh.app.sales.vo.SalesVo;
+import com.kh.app.util.db.JDBCTemplate;
+
+public class HomeService {
+
+	public Map<String , Object>  selectItemList() throws Exception {
+		
+		// conn 
+		Connection conn = JDBCTemplate.getConnection();
+		
+		HomeDao dao = new HomeDao();
+		List<String> bestReviewNoList = dao.selectBestReviewCount(conn);
+		List<String> bestOrderNoList = dao.selectBestOrderCount(conn);
+		List<String> bestReviewLikeNoList = dao.selectBestReviewLikeCount(conn);
+		
+		// 리뷰 많은순
+		List<SalesVo> salesBestReviewVoList = dao.selectItemList(bestReviewNoList,conn);
+		List<SalesVo> salesBestOrderVoList = dao.selectItemList(bestOrderNoList,conn);
+		List<SalesVo> salesBestReviewLikeVoList = dao.selectItemList(bestReviewLikeNoList,conn);
+		
+		Map<String , Object> map = new HashMap<>();
+		
+		map.put("salesBestReviewVoList", salesBestReviewVoList);
+		map.put("salesBestOrderVoList", salesBestOrderVoList);
+		map.put("salesBestReviewLikeVoList", salesBestReviewLikeVoList);
+		
+		JDBCTemplate.close(conn);
+		
+		return map;
+		
+	}
+
+	
+	
+}
